36dfbc5e6073699871c9e1fd2324cc0653dc9e39
kotmw0701 <supurasyu@hotmail.co.jp>
2018-12-07 12:24:10 +0900

    UIの作成

--- Tello/GamePadManager.cs
+++ Tello/GamePadManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SlimDX.XInput;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +9,69 @@ namespace Tello {
     class GamePadManager {
         /*
          * メモ
-         * RightX → 右に進む / 左に進む
+         * RightX → 右に進む / 左に進む (MAX +-660)
          * RightY → 前進 / 後退
          * LeftX → 右回転 / 左回転
          * LeftY → 上昇 / 下降
-         * LTrigger → 低速モード   MAX(330-255) DEF(110-0)
-         * RTrigget → 仮高速モード MAX(660-255) DEF(110-0)
+         * LTrigger → 低速モード   MAX(330-1.0) DEF(110-0.0)
+         * RTrigget → 仮高速モード MAX(660-1.0) DEF(110-0.0)
          * RightStick → 押し込みで入力無効化(というかそうしないと数値が安定しない(パッド壊れてんじゃん
          * LeftStick → 同上
          * 
          * 高速モード
          * RTrigger押し込み→LTrigger押し込み？
          * 
-         * 
+         * (ずっと割ってやろうと思ってたけど，これよく考えたら割合取ったほうが早いやんかなってなった)
          */
-        public short LeftX { get; set; }
-        public short LeftY { get; set; }
-        public short RightX { get; set; }
-        public short RightY { get; set; }
-        public byte LTrigger { get; set; }
-        public byte RTrigget { get; set; }
+        /// <summary>
+        /// 1.0 - 0.0 の範囲で帰ってきます
+        /// </summary>
+        public double LeftX { get; private set; }
+        /// <summary>
+        /// 1.0 - 0.0 の範囲で帰ってきます
+        /// </summary>
+        public double LeftY { get; private set; }
+        /// <summary>
+        /// 1.0 - 0.0 の範囲で帰ってきます
+        /// </summary>
+        public double RightX { get; private set; }
+        /// <summary>
+        /// 1.0 - 0.0 の範囲で帰ってきます
+        /// </summary>
+        public double RightY { get; private set; }
+        /// <summary>
+        /// 1.0 - 0.0 の範囲で帰ってきます
+        /// </summary>
+        public double LTrigger { get; private set; }
+        /// <summary>
+        /// 1.0 - 0.0 の範囲で帰ってきます
+        /// </summary>
+        public double RTrigger { get; private set; }
+        public bool IsFastMode { get; private set; }
 
+        private readonly Controller controller;
 
+        public GamePadManager() {
+            controller = new Controller(UserIndex.One);
+        }
+
+        public void StickStreaming() {
+
+            var pad = controller.GetState().Gamepad;
+            bool onLeftThumb = pad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb);
+            bool onRightThumb = pad.Buttons.HasFlag(GamepadButtonFlags.RightThumb);
+            if (onLeftThumb) LeftX = LeftY = 0.0;
+            else {
+                LeftX = Math.Round(pad.LeftThumbX / 32767.0 * 100) / 100;
+                LeftY = Math.Round(pad.LeftThumbY / 32767.0 * 100) / 100;
+            }
+            if (onRightThumb) RightX = RightY = 0.0;
+            else {
+                RightX = Math.Round(pad.RightThumbX / 32767.0 * 100) / 100;
+                RightY = Math.Round(pad.RightThumbY / 32767.0 * 100) / 100;
+            }
+            LTrigger = pad.LeftTrigger / 255;
+            RTrigger = pad.RightTrigger / 255;
+        }
     }
 }
---
+++ Tello/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tello {
+    class ViewModel : INotifyPropertyChanged {
+        private bool _fastMode;
+        private short _rotation;
+        private short _throttle;
+        private short _pitch;
+        private short _role;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaizePropertyChanged([CallerMemberName]string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public bool FastMode {
+            get => _fastMode;
+            set {
+                _fastMode = value;
+                RaizePropertyChanged(nameof(FastMode));
+            }
+        }
+        public short Rotation {
+            get => _rotation;
+            set {
+                _rotation = value;
+                RaizePropertyChanged(nameof(Rotation));
+            }
+        }
+        public short Throttle {
+            get => _throttle;
+            set {
+                _throttle = value;
+                RaizePropertyChanged(nameof(Throttle));
+            }
+        }
+        public short Pitch {
+            get => _pitch;
+            set {
+                _pitch = value;
+                RaizePropertyChanged(nameof(Pitch));
+            }
+        }
+        public short Role {
+            get => _role;
+            set {
+                _role = value;
+                RaizePropertyChanged(nameof(Role));
+            }
+        }
+    }
+}
